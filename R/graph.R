312de025ce36b2ee2f6776d30ff31bb5cedfa0ee
jwons <joe.wonsil@gmail.com>
2018-06-07 13:49:18 -0400

    reformat

--- R/graph.R
+++ R/graph.R
@@ -1,20 +1,22 @@
 library(provParseR)
 library(igraph)
 
-prov.parse("./inst/testdata/ddg.json")
-
-node.names <- c(get.proc.nodes()$'label', get.data.nodes()$'label', get.func.nodes()$'label', get.libs()$'label')
-edges <- as.matrix(rbind(get.proc.data(), get.data.proc(), get.func.proc()))[,c("activity","entity")]
-adj.graph <- matrix(0, nrow = length(node.names), ncol = length(node.names))
-rownames(adj.graph) <- colnames(adj.graph) <- node.names
-adj.graph[edges] <- 1
 
 
-
-
-create.graph <- function(nodes, req.edges){
-  node.names <- unlist(lapply(nodes, rownames), use.names = F)
-  edges <- as.matrix(do.call(rbind, req.edges))
+create.graph <- function(){
+  
+  prov.parse("./inst/testdata/ddg.json")
+  
+  node.names <- c(get.proc.nodes()$'label', 
+                  get.data.nodes()$'label', 
+                  get.func.nodes()$'label', 
+                  get.libs()$'label')
+  
+  edges <- as.matrix(rbind(get.proc.data(), 
+                           get.data.proc(), 
+                           get.func.proc())
+  )[, c("activity","entity")]
+  
   adj.graph <- matrix(0, nrow = length(node.names), ncol = length(node.names))
   rownames(adj.graph) <- colnames(adj.graph) <- node.names
   adj.graph[edges] <- 1
