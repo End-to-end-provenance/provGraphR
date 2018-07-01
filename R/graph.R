92fd5f027a02eae8b695781a2d28b023a08ce2b7
jwons <joe.wonsil@gmail.com>
2018-07-01 11:57:18 -0400

    switch back

--- R/graph.R
+++ R/graph.R
@@ -32,7 +32,7 @@ graph.env$adj.graph <- NULL
   )[,c("entity", "activity")]
 
   # Create the graph, populating each element with zeros by the length of nodes
-  adj.graph <- matrix(0, nrow = length(labels), ncol = length(labels))
+  adj.graph <- Matrix(0, nrow = length(labels), ncol = length(labels), sparse =T)
 
   # Make sure the resulting matrix is labelled for grabbing the right node
   rownames(adj.graph) <- colnames(adj.graph) <- labels
