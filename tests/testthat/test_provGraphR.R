98dbc99d6afb2ff339ccb9571c85ae36c18d020d
fong22e <fong22e@mtholyoke.edu>
2019-06-07 12:18:22 -0400

    updated testthat test

--- tests/testthat/test_provGraphR.R
+++ tests/testthat/test_provGraphR.R
@@ -2,6 +2,13 @@ library(provGraphR)
 library(provParseR)
 library(testthat)
 
+# no procedure nodes
+test_that( "no procedure nodes", {
+	test.data.file <- system.file("testdata", "empty.json", package = "provGraphR")
+	expect_warning(create.graph(test.data.file))
+	expect_null(create.graph(test.data.file))
+})
+
 ## Loading test data
 test.data.file <- system.file("testdata", "prov.json", package = "provGraphR")
 #prov.parse(test.data.file)
